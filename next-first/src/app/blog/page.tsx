4ecfd0a617662064de0ef446295afa9ac3098e5c
ariesdimasy <ariesdimasy>
2024-03-08 07:06:55 +0700

    update next

--- next-first/src/app/blog/page.tsx
+++ next-first/src/app/blog/page.tsx
@@ -1,8 +1,57 @@
+"use client";
+
+import axios from "axios";
+import dynamic from "next/dynamic";
+import { useQuery } from "react-query";
+
+async function getData() {
+  const initialData = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  return initialData;
+}
+
+export const revalidate = 3600;
+
+const Parent = dynamic(() => import("@/components/parent"));
+
 export default function Blog() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getData,
+  });
+
   return (
-    <div>
-      <h1 className="text-xl"> Hello World Blog </h1>
+    <div className="w-[80%] mx-auto my-[20px]">
+      <h1 className="text-xl"> List Blog </h1>
       <hr></hr>
+
+      <Parent />
+
+      {isLoading && <div className="text-lg">Loading ... </div>}
+      {!isLoading && (
+        <div style={{ width: "80%", margin: "auto" }}>
+          {data?.data?.map(
+            (
+              item: { id: number; title: string; body: string },
+              index: number
+            ) => {
+              return (
+                <a
+                  style={{ margin: "10px 0", display: "block" }}
+                  key={index}
+                  href={`/blog/${item.id}`}
+                >
+                  <div>
+                    {item.id} - {item.title}
+                  </div>
+                </a>
+              );
+            }
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- next-first/src/app/product/page.tsx
+++ next-first/src/app/product/page.tsx
@@ -1,14 +1,39 @@
-import Parent from "@/components/parent";
-import Test from "@/components/test";
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-export default function Product() {
+  if (!res.ok) {
+    throw new Error("failed fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Product() {
+  const data = await getData();
+
   return (
     <div>
       <h1> Product </h1>
-      <div></div>
-      <Parent className="bg-blue-500">
-        <Test></Test>
-      </Parent>
+      <div style={{ width: "80%", margin: "auto" }}>
+        {data.map(
+          (
+            item: { id: number; title: string; body: string },
+            index: number
+          ) => {
+            return (
+              <a
+                style={{ margin: "10px 0", display: "block" }}
+                key={index}
+                href={`/product/${item.id}`}
+              >
+                <div>
+                  {item.id} - {item.title}
+                </div>
+              </a>
+            );
+          }
+        )}
+      </div>
     </div>
   );
 }
